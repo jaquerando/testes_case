9935b0f16cb6d6af355e3a5b83e1f88de9327b5f
jaquerando <jaquerando@gmail.com>
2024-11-19 00:09:20 -0300

    Update silver.py

--- silver.py
+++ silver.py
@@ -8,7 +8,7 @@ from google.cloud import bigquery
 from google.cloud import storage
 import pandas as pd
 
-# Configuração de autenticação e e-mail para falhas
+# Authentication and email configuration for failures
 def alert_email_on_failure(context):
     dag_id = context.get('dag').dag_id
     task_id = context.get('task_instance').task_id
@@ -16,40 +16,40 @@ def alert_email_on_failure(context):
     log_url = context.get('task_instance').log_url
     email = "jaquerando@gmail.com"
 
-    subject = f"Alerta de Falha - DAG: {dag_id}, Task: {task_id}"
+    subject = f"Failure Alert - DAG: {dag_id}, Task: {task_id}"
     body = f"""
-    <h3>Alerta de Falha na DAG</h3>
+    <h3>DAG Failure Alert</h3>
     <p><strong>DAG:</strong> {dag_id}</p>
-    <p><strong>Task com Falha:</strong> {task_id}</p>
-    <p><strong>Data de Execução:</strong> {execution_date}</p>
-    <p><strong>URL do Log:</strong> <a href="{log_url}">{log_url}</a></p>
-    <p>Verifique o log para detalhes da falha.</p>
+    <p><strong>Failed Task:</strong> {task_id}</p>
+    <p><strong>Execution Date:</strong> {execution_date}</p>
+    <p><strong>Log URL:</strong> <a href="{log_url}">{log_url}</a></p>
+    <p>Check the log for failure details.</p>
     """
     send_email(to=email, subject=subject, html_content=body)
 
-# Sensor para verificar se o arquivo foi atualizado na Bronze
+# Sensor to check if the file was updated in Bronze
 def check_file_updated(**kwargs):
     updated = kwargs['ti'].xcom_pull(
-        dag_id='bronze_dag',  # Nome da DAG Bronze
-        task_ids='fetch_data_and_compare',  # Task que define o XCom
+        dag_id='bronze_dag',  # Name of the Bronze DAG
+        task_ids='fetch_data_and_compare',  # Task that defines the XCom
         key='file_updated'
     )
     
-    # Verifica se o valor retornado é válido
+    # Checks if the returned value is valid
     if updated is None:
-        logging.warning("Nenhum valor encontrado no XCom. Abortando execução.")
+        logging.warning("No value found in XCom. Aborting execution.")
         return False
     
     if updated:
-        logging.info("Recebido sinal no XCom para executar a DAG Silver: Executando.")
+        logging.info("Signal received in XCom to run the Silver DAG: Running.")
         return True
     else:
-        logging.info("Recebido sinal no XCom para NÃO executar a DAG Silver: Abortando.")
+        logging.info("Signal received in XCom to NOT run the Silver DAG: Aborting.")
         return False
 
-# Função para baixar os dados do GCS
+# Function to download data from GCS
 def download_data(**kwargs):
-    log_messages = ["Iniciando o download dos dados da camada Bronze"]
+    log_messages = ["Starting download of data from the Bronze layer"]
     try:
         bucket_name = "bucket-case-abinbev"
         storage_client = storage.Client()
@@ -57,16 +57,16 @@ def download_data(**kwargs):
         blob = bucket.blob("data/bronze/breweries_raw.ndjson")
         raw_data = blob.download_as_text()
         kwargs['ti'].xcom_push(key="raw_data", value=raw_data)
-        log_messages.append("Download concluído com sucesso.")
+        log_messages.append("Download completed successfully.")
     except Exception as e:
-        log_messages.append(f"Erro ao baixar os dados: {e}")
-        logging.error(f"Erro: {e}")
+        log_messages.append(f"Error downloading data: {e}")
+        logging.error(f"Error: {e}")
         raise
     save_log(log_messages)
 
-# Função para transformar os dados
+# Function to transform the data
 def transform_data(**kwargs):
-    log_messages = ["Iniciando a transformação dos dados"]
+    log_messages = ["Starting data transformation"]
     try:
         raw_data = kwargs['ti'].xcom_pull(key="raw_data", task_ids="download_data")
         raw_df = pd.read_json(raw_data, lines=True)
@@ -75,16 +75,16 @@ def transform_data(**kwargs):
         raw_df["brewery_type"] = raw_df["brewery_type"].fillna("unknown").astype(str)
         raw_df["state_partition"] = raw_df["state"].apply(lambda x: hash(x) % 50)
         kwargs['ti'].xcom_push(key="transformed_data", value=raw_df.to_json(orient='records', lines=True))
-        log_messages.append("Transformação concluída com sucesso.")
+        log_messages.append("Transformation completed successfully.")
     except Exception as e:
-        log_messages.append(f"Erro na transformação dos dados: {e}")
-        logging.error(f"Erro: {e}")
+        log_messages.append(f"Error transforming data: {e}")
+        logging.error(f"Error: {e}")
         raise
     save_log(log_messages)
 
-# Função para carregar os dados no BigQuery
+# Function to load data into BigQuery
 def load_data_to_bigquery(**kwargs):
-    log_messages = ["Iniciando o carregamento dos dados para o BigQuery"]
+    log_messages = ["Starting data loading to BigQuery"]
     try:
         transformed_data = kwargs['ti'].xcom_pull(key="transformed_data", task_ids="transform_data")
         transformed_df = pd.read_json(transformed_data, lines=True)
@@ -94,24 +94,25 @@ def load_data_to_bigquery(**kwargs):
         client = bigquery.Client()
         table_ref = client.dataset(dataset_id).table(table_id)
         job_config = bigquery.LoadJobConfig(write_disposition=bigquery.WriteDisposition.WRITE_APPEND)
-        job = client.load_table_from_dataframe(transformed_df, table_ref, job_config=job_config)
+        job = client.load_table_from_dataframe(transformed_df,   
+ table_ref, job_config=job_config)
         job.result()
-        log_messages.append(f"Dados carregados com sucesso na tabela {dataset_id}.{table_id} no BigQuery.")
+        log_messages.append(f"Data successfully loaded into table {dataset_id}.{table_id} in BigQuery.")
     except Exception as e:
-        log_messages.append(f"Erro ao carregar os dados no BigQuery: {e}")
-        logging.error(f"Erro: {e}")
+        log_messages.append(f"Error loading data into BigQuery: {e}")
+        logging.error(f"Error: {e}")
         raise
     save_log(log_messages)
 
-# Função para salvar logs
+# Function to save logs
 def save_log(messages):
     client = storage.Client()
     log_bucket = client.get_bucket("us-central1-composer-case-e66c77cc-bucket")
     log_blob = log_bucket.blob(f'logs/silver_dag_log_{datetime.utcnow().strftime("%Y%m%d%H%M%S")}.log')
     log_blob.upload_from_string("\n".join(messages), content_type="text/plain")
-    logging.info("Log salvo no bucket de logs.")
+    logging.info("Log saved to log bucket.")
 
-# Configuração da DAG Silver
+# Silver DAG configuration
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -124,38 +125,38 @@ default_args = {
 with DAG(
     'silver_dag',
     default_args=default_args,
-    description='DAG para transformar dados da camada Bronze e carregar na camada Silver no BigQuery',
+    description='DAG to transform data from the Bronze layer and load it into the Silver layer in BigQuery',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
     
-    # Sensor para verificar se a DAG Bronze detectou atualização
+    # Sensor to check if the Bronze DAG detected an update
     wait_for_file_update = PythonSensor(
         task_id='wait_for_file_update',
         python_callable=check_file_updated,
         poke_interval=30,
-        timeout=600,  # Tempo máximo de espera
+        timeout=600,  # Maximum waiting time
         mode='poke',
     )
     
-    # Task de download
+    # Download task
     download_data_task = PythonOperator(
         task_id='download_data',
         python_callable=download_data,
     )
     
-    # Task de transformação
+    # Transformation task
     transform_data_task = PythonOperator(
         task_id='transform_data',
         python_callable=transform_data,
     )
     
-    # Task de carregamento
+    # Loading task
     load_data_task = PythonOperator(
         task_id='load_data_to_bigquery',
         python_callable=load_data_to_bigquery,
     )
 
-    # Definindo a sequência de execução
+    # Defining the execution sequence
     wait_for_file_update >> download_data_task >> transform_data_task >> load_data_task
